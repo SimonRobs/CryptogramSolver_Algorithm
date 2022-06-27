81d273091f618882a802fd2149d62ea008da97c8
Simon Robatto <simon.robatto-simard@polymtl.ca>
2022-06-27 14:26:40 -0400

    Initial Commit

---
+++ CryptogramSolver/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CryptogramSolver
+//
+//  Created by Simon Robatto on 2022-06-27.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
